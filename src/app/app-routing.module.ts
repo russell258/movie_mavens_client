c4628415459c4dfc772256b95f0685dcf046bf8b
russell258 <duckie258@gmail.com>
2023-09-05 20:53:42 +0800

    base routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './security/login/login.component';
+import { RegistrationComponent } from './security/registration/registration.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'login', component: LoginComponent},
+  { path: 'register', component: RegistrationComponent},
+  { path: '**', redirectTo: '/login', pathMatch: 'full'}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
