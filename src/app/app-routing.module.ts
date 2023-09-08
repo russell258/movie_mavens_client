57157a8f2512b0d74b3313b6b6e546aa9b3b799c
russell258 <duckie258@gmail.com>
2023-09-08 15:34:32 +0800

    added dynamic routing to write reviews

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { LoginComponent } from './security/login/login.component';
 import { RegistrationComponent } from './security/registration/registration.component';
 import { ReviewComponent } from './reviews/review/review.component';
 import { MovieComponent } from './movies/movie/movie.component';
+import { WriteComponent } from './reviews/write/write.component';
 
 const routes: Routes = [
   { path: 'login', component: LoginComponent},
   { path: 'register', component: RegistrationComponent},
   { path: 'reviews', component: ReviewComponent},
   { path: 'home', component: MovieComponent},
-  { path: '**', redirectTo: '/login', pathMatch: 'full'}
+  { path: 'write/:id', component: WriteComponent},
+  // { path: '**', redirectTo: '/login', pathMatch: 'full'}
 ];
 
 @NgModule({
