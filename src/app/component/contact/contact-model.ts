fa2ec5f60937f0fbfef83d9407c8f88fc87588bd
russell258 <duckie258@gmail.com>
2023-09-14 04:32:28 +0800

    send mail and save contact for enquiries

---
+++ src/app/component/contact/contact-model.ts
@@ -0,0 +1,6 @@
+export interface ContactModel {
+  userid?: string;
+  fullName: string;
+  email: string;
+  message: string;
+}
--- src/app/component/contact/contact.component.ts
+++ src/app/component/contact/contact.component.ts
@@ -1,6 +1,7 @@
 import { Component, ViewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { HttpService } from 'src/app/security/login/http.service';
+import { ContactModel } from './contact-model';
 
 
 @Component({
@@ -16,20 +17,24 @@ export class ContactComponent {
 
   constructor(private fb: FormBuilder, private httpService: HttpService){}
 
+  userid:string = window.sessionStorage.getItem("user_id");
+
   ngOnInit():void{
     this.contactForm = this.fb.group({
+      uid: this.fb.control<string>(this.userid),
       fullName: this.fb.control<string>('',[Validators.required]),
       email: this.fb.control<string>('',[Validators.required,Validators.email]),
       message: this.fb.control<string>('',[Validators.required])
     })
   }
 
+
   contactSubmit(){
-    this.httpService.request('POST','/api/contact',this.contactForm.value).subscribe({
+    this.httpService.request('POST','/api/contact',this.contactForm.value as ContactModel).subscribe({
       next: (data)=>{
         let jsonObj = JSON.stringify(data);
-        //see what happens if put an invalid email.
-        alert(`We have received your message. For any follow up, we will contact you through your email: ${JSON.parse(jsonObj)['reviewId']}`);
+        //test what happens if put an invalid email.
+        alert(`Hi `+ this.contactForm.value.fullName + `, we have received your message. For any follow up, we will contact you through your email: ${JSON.parse(jsonObj)['email']}`);
       },
     error: (e) => {
       console.error(e);
@@ -46,14 +51,14 @@ export class ContactComponent {
       mapTypeId: google.maps.MapTypeId.ROADMAP,
   };
 
-  this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-  const marker = new google.maps.Marker({
-    position: new google.maps.LatLng(1.2923210380975625, 103.7765463514608),
-    map: this.map,
-    title: "NUS ISS",
-    draggable: true,
-    animation: google.maps.Animation.DROP
-});
+    this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
+    const marker = new google.maps.Marker({
+      position: new google.maps.LatLng(1.2923210380975625, 103.7765463514608),
+      map: this.map,
+      title: "NUS ISS",
+      draggable: true,
+      animation: google.maps.Animation.DROP
+    });
   }
 
 }
